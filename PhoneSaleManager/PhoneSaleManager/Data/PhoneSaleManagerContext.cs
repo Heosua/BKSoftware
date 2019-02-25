22ef3e239e94423a5402e91b32dbc9e5001d65ef
Ho Minh Sang <38745048+Heosua@users.noreply.github.com>
2019-02-25 18:01:14 +0700

    asp.net FirstApplication

---
+++ PhoneSaleManager/PhoneSaleManager/Data/PhoneSaleManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhoneSaleManager.Models
+{
+    public class PhoneSaleManagerContext : DbContext
+    {
+        public PhoneSaleManagerContext (DbContextOptions<PhoneSaleManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PhoneSaleManager.Models.Movie> Movie { get; set; }
+    }
+}
