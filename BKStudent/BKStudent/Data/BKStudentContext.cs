ab184a197dc1197000f3951662a1a907cdb49902
Ho Minh Sang <38745048+Heosua@users.noreply.github.com>
2019-02-25 18:06:53 +0700

    submit again

---
+++ BKStudent/BKStudent/Data/BKStudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BKStudent.Models
+{
+    public class BKStudentContext : DbContext
+    {
+        public BKStudentContext (DbContextOptions<BKStudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BKStudent.Models.Student> Student { get; set; }
+    }
+}
---
+++ BKStudent/BKStudent/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BKStudent.Models
+{
+    public class Student { 
+    
+        public int      Id { get; set;} 
+        public string   Name { get; set; }
+        public string   Faculty { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        public int      CPA { get; set; }
+    }
+}
